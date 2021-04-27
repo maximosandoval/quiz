f3aa8afadbe33e5fea8434a777cc9069e471809c
Maximo Sandoval <80641742+maximosandoval@users.noreply.github.com>
2021-04-27 14:28:25 -0600

    Initial commit of quiz
    1. Initial commit of folders and files for quiz.

---
+++ script/script.js
@@ -0,0 +1,72 @@
+//VAR DECLARATIONS
+var timeElement = document.querySelector.Selector(".timer");
+carousel = document.querySelector(".carouselbox");
+var next = carousel.querySelector(".next");
+var prev = carousel.querySelector(".prev");
+var score = 0;
+var time = 120
+var index = 0;
+var currentImage;
+
+//Start quiz (button)
+document.querySelector(#start-btn).addEventListener
+("click", function () {
+
+//start timer
+timer = setInterval(function () {
+    time--;
+
+//show time
+document.querySelector(".time").textContent = time;
+//timer
+if (time <= 0) {
+    clearInterval(timer);
+    endgame();
+}, 1000)
+}
+
+
+
+//Quiz Questions
+//Capture the end user click and compare their answer to the right answer
+
+
+
+//NAVIGATION (SEE LESSON 17)
+INDEX = INDEX + DIRECTION;
+} else if (index >)
+
+//Set up array of question objects
+
+//TIMER PRESENTATION
+
+
+//SUBMIT ANSWER CORRECTLY
+
+
+//SUBMIT ANSWER INCORRECTLY
+
+
+//INCORRECT ANSWER TIMER REDUCED BY 3 SECONDS
+
+
+//Add to score for correct
+
+//Enter Initials for high score & display high score
+
+
+//ALL QUESTIONS ANSWERED TIMER EQUALS "0"
+
+//GAME OVER MAN!
+
+
+//CLEAR HIGH SCORE BUTTON
+clearHighScores.addEventListner("click", function (event) {);
+
+
+//SAVE TEST WITH INITIALS AND SCORE
+
+
+
+
+
